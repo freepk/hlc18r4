b996a18b36aa798916019727541944598198d98b
freepk <freepk@mail.ru>
2019-01-01 01:52:13 +0000

    Slicing

--- arch.go
+++ arch.go
@@ -7,28 +7,50 @@ import (
 	"sync"
 )
 
+const jsonSkip = 14
+
 func readFile(wait *sync.WaitGroup, file *zip.File) {
 	if wait != nil {
 		defer wait.Done()
 	}
+	println(file.Name)
 	r, err := file.Open()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer r.Close()
-	b := make([]byte, 4096)
+	p := 0
+	x := 14
+	b := make([]byte, 8192)
 	for {
-		n, err := r.Read(b)
+		n, err := r.Read(b[p:])
+		if n > 0 {
+			n += p
+			c := 0
+			i := 0
+			j := x
+			for j < n {
+				switch b[j] {
+				case '{':
+					c++
+					if c == 1 {
+						i = j
+					}
+				case '}':
+					c--
+					if c == 0 {
+					}
+				}
+				j++
+			}
+			x = 0
+			p = copy(b, b[i:n])
+		}
 		if err == io.EOF {
-			break
+			return
 		}
 		if err != nil {
 			log.Fatal(err)
-		}
-		for i := 0; i < n; i++ {
-			c := b[i]
-			if c > 0 {
-			}
 		}
 	}
 }
