8e91f4846f526c1b1480a4966cb6c54ffc019b9d
freepk <freepk@mail.ru>
2019-01-02 02:58:40 +0000

    Clenaup

--- arch_test.go
+++ arch_test.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"archive/zip"
+	//"fmt"
+	"sync"
 	"testing"
 )
 
 func TestReadArchive(t *testing.T) {
-	readArchive("data/data.zip")
+	z, err := zip.OpenReader("data/data.zip")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer z.Close()
+	n := len(z.File)
+	w := new(sync.WaitGroup)
+	w.Add(n)
+	for i := 0; i < n; i++ {
+		r, err := z.File[i].Open()
+		if err != nil {
+			t.Fatal(err)
+		}
+		go func() {
+			defer w.Done()
+			defer r.Close()
+			b := make([]byte, 8192)
+			jsonReadObj(r, b, 14, func(b []byte) error {
+				//fmt.Println(string(b))
+				return nil
+			})
+		}()
+	}
+	w.Wait()
 }
