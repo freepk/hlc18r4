ccd6cf914873d9470e42be3b578ee26e6793f585
freepk <freepk@mail.ru>
2019-01-03 01:54:23 +0000

    Good refactor

--- utf8.go
+++ utf8.go
@@ -1,11 +1,7 @@
 package main
 
-import (
-	"unicode/utf8"
-)
-
-func utf8Unhex(b byte) (rune, bool) {
-	c := rune(b)
+func utf8Unhex(b byte) (int, bool) {
+	c := int(b)
 	switch {
 	case '0' <= c && c <= '9':
 		return c - '0', true
@@ -17,43 +13,48 @@ func utf8Unhex(b byte) (rune, bool) {
 	return 0, false
 }
 
-func utf8UnquoteChar(b []byte) (rune, bool) {
-	if len(b) < 6 {
-		return 0, false
+func utf8UnquoteChar(a, b, c, d byte) (byte, byte, bool) {
+	va, ok := utf8Unhex(a)
+	if !ok {
+		return 0, 0, false
 	}
-	if b[0] != '\\' {
-		return 0, false
+	va <<= 12
+	vb, ok := utf8Unhex(b)
+	if !ok {
+		return 0, 0, false
 	}
-	if b[1] != 'u' {
-		return 0, false
+	vb <<= 8
+	vc, ok := utf8Unhex(c)
+	if !ok {
+		return 0, 0, false
 	}
-	r := rune(0)
-	for i := 2; i < 6; i++ {
-		x, ok := utf8Unhex(b[i])
-		if !ok {
-			return 0, false
-		}
-		r = (r << 4) | x
+	vc <<= 4
+	vd, ok := utf8Unhex(d)
+	if !ok {
+		return 0, 0, false
 	}
-	return r, true
+	v := va + vb + vc + vd
+	x := []byte(string(v))
+	return x[0], x[1], true
 }
 
-func utf8Unquote(d, s []byte) int {
-	n := len(s)
-	if n > len(d) {
-		return 0
-	}
+func utf8Unquote(dst, src []byte) int {
+	n := len(src)
 	i := 0
 	j := 0
 	for i < n {
-		c, ok := utf8UnquoteChar(s[i:])
-		if ok {
-			z := utf8.EncodeRune(d[j:], c)
-			j += z
-			i += 6
-			continue
+		if i+6 <= n && src[i] == '\\' && src[i+1] == 'u' {
+			a, b, ok := utf8UnquoteChar(src[i+2], src[i+3], src[i+4], src[i+5])
+			if ok {
+				dst[j] = a
+				j++
+				dst[j] = b
+				j++
+				i += 6
+				continue
+			}
 		}
-		d[j] = s[i]
+		dst[j] = src[i]
 		i++
 		j++
 	}
