81c63ba3e01aa8334e111dbc7d4f80c1d3c9354c
freepk <freepk@mail.ru>
2019-01-05 03:39:40 +0000

    Testing

--- country_test.go
+++ country_test.go
@@ -4,6 +4,16 @@ import (
 	"testing"
 )
 
+func TestCountry(t *testing.T) {
+	for i := 1; i <= 70; i++ {
+		name := getCountryName(i)
+		if getCountryId(name) != i {
+			t.Log(name)
+			t.Fail()
+		}
+	}
+}
+
 func BenchmarkGetCountryName(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		getCountryName(55)
@@ -12,6 +22,6 @@ func BenchmarkGetCountryName(b *testing.B) {
 
 func BenchmarkGetCountryId(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		getCountryId("Росция")
+		getCountryId("Алания")
 	}
 }
