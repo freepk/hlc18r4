bbd0fab67350741808fa404688f40237b2baae19
Pavel Chabinsky <freepk@mail.ru>
2019-01-08 15:09:50 +0300

    Account refactor

--- account.go
+++ account.go
@@ -1,22 +1,26 @@
 package main
 
+//import (
+//	"fmt"
+//)
+
 type Like struct {
 	ID int
 	TS int
 }
 
 type Account struct {
-	Sex           []byte
-	Email         []byte
-	Sname         []byte
-	Fname         []byte
-	Status        []byte
-	City          []byte
-	Country       []byte
-	Phone         []byte
-	Birth         int
 	ID            int
+	Birth         int
 	Joined        int
+	Email         []byte
+	Fname         []byte
+	Sname         []byte
+	Phone         []byte
+	Sex           []byte
+	Country       []byte
+	City          []byte
+	Status        []byte
 	PremiumStart  int
 	PremiumFinish int
 	Interests     [][]byte
@@ -24,21 +28,25 @@ type Account struct {
 }
 
 func (acc *Account) Reset() {
-	acc.Sex = acc.Sex[:0]
-	acc.Email = acc.Email[:0]
-	acc.Sname = acc.Sname[:0]
-	acc.Fname = acc.Fname[:0]
-	acc.Status = acc.Status[:0]
-	acc.City = acc.City[:0]
-	acc.Country = acc.Country[:0]
-	acc.Phone = acc.Phone[:0]
-	acc.Birth = 0
 	acc.ID = 0
+	acc.Birth = 0
 	acc.Joined = 0
+	acc.Email = acc.Email[:0]
+	acc.Fname = acc.Fname[:0]
+	acc.Sname = acc.Sname[:0]
+	acc.Phone = acc.Phone[:0]
+	acc.Sex = acc.Sex[:0]
+	acc.Country = acc.Country[:0]
+	acc.City = acc.City[:0]
+	acc.Status = acc.Status[:0]
 	acc.PremiumStart = 0
 	acc.PremiumFinish = 0
 	acc.Interests = acc.Interests[:0]
 	acc.Likes = acc.Likes[:0]
+}
+
+func (acc *Account) Tokens() []int {
+	return nil
 }
 
 func (acc *Account) Parse(b []byte) ([]byte, bool) {
@@ -54,6 +62,37 @@ func (acc *Account) Parse(b []byte) ([]byte, bool) {
 	for {
 		t = parseSpaces(t)
 		switch {
+
+		case len(t) > accIdLen && string(t[:accIdLen]) == accIdKey:
+			if acc.ID, t, ok = parseInt(t[accIdLen:]); !ok {
+				return b, false
+			}
+			// println("\tID", acc.ID)
+		case len(t) > accBirthLen && string(t[:accBirthLen]) == accBirthKey:
+			if acc.Birth, t, ok = parseInt(t[accBirthLen:]); !ok {
+				return b, false
+			}
+			// println("\tBirth", acc.Birth)
+		case len(t) > accJoinedLen && string(t[:accJoinedLen]) == accJoinedKey:
+			if acc.Joined, t, ok = parseInt(t[accJoinedLen:]); !ok {
+				return b, false
+			}
+			// println("\tJoined", acc.Joined)
+		case len(t) > accEmailLen && string(t[:accEmailLen]) == accEmailKey:
+			if acc.Email, t, ok = parsePhrase(t[accEmailLen:], '"'); !ok {
+				return b, false
+			}
+			// println("\tEmail", string(acc.Email))
+		case len(t) > accFnameLen && string(t[:accFnameLen]) == accFnameKey:
+			if acc.Fname, t, ok = parsePhrase(t[accFnameLen:], '"'); !ok {
+				return b, false
+			}
+			// println("\tFname", string(acc.Fname))
+		case len(t) > accSnameLen && string(t[:accSnameLen]) == accSnameKey:
+			if acc.Sname, t, ok = parsePhrase(t[accSnameLen:], '"'); !ok {
+				return b, false
+			}
+			// println("\tSname", string(acc.Sname))
 		case len(t) > accPhoneLen && string(t[:accPhoneLen]) == accPhoneKey:
 			if acc.Phone, t, ok = parsePhrase(t[accPhoneLen:], '"'); !ok {
 				return b, false
@@ -64,51 +103,21 @@ func (acc *Account) Parse(b []byte) ([]byte, bool) {
 				return b, false
 			}
 			// println("\tSex", string(acc.Sex))
-		case len(t) > accEmailLen && string(t[:accEmailLen]) == accEmailKey:
-			if acc.Email, t, ok = parsePhrase(t[accEmailLen:], '"'); !ok {
-				return b, false
-			}
-			// println("\tEmail", string(acc.Email))
-		case len(t) > accSnameLen && string(t[:accSnameLen]) == accSnameKey:
-			if acc.Sname, t, ok = parsePhrase(t[accSnameLen:], '"'); !ok {
-				return b, false
-			}
-			// println("\tSname", string(acc.Sname))
-		case len(t) > accFnameLen && string(t[:accFnameLen]) == accFnameKey:
-			if acc.Fname, t, ok = parsePhrase(t[accFnameLen:], '"'); !ok {
-				return b, false
-			}
-			// println("\tFname", string(acc.Fname))
-		case len(t) > accStatusLen && string(t[:accStatusLen]) == accStatusKey:
-			if acc.Status, t, ok = parsePhrase(t[accStatusLen:], '"'); !ok {
-				return b, false
-			}
-			// println("\tStatus", string(acc.Status))
-		case len(t) > accCityLen && string(t[:accCityLen]) == accCityKey:
-			if acc.City, t, ok = parsePhrase(t[accCityLen:], '"'); !ok {
-				return b, false
-			}
-			// println("\tCity", string(acc.City))
 		case len(t) > accCountryLen && string(t[:accCountryLen]) == accCountryKey:
 			if acc.Country, t, ok = parsePhrase(t[accCountryLen:], '"'); !ok {
 				return b, false
 			}
 			// println("\tCountry", string(acc.Country))
-		case len(t) > accBirthLen && string(t[:accBirthLen]) == accBirthKey:
-			if acc.Birth, t, ok = parseInt(t[accBirthLen:]); !ok {
+		case len(t) > accCityLen && string(t[:accCityLen]) == accCityKey:
+			if acc.City, t, ok = parsePhrase(t[accCityLen:], '"'); !ok {
 				return b, false
 			}
-			// println("\tBirth", acc.Birth)
-		case len(t) > accIdLen && string(t[:accIdLen]) == accIdKey:
-			if acc.ID, t, ok = parseInt(t[accIdLen:]); !ok {
+			// println("\tCity", string(acc.City))
+		case len(t) > accStatusLen && string(t[:accStatusLen]) == accStatusKey:
+			if acc.Status, t, ok = parsePhrase(t[accStatusLen:], '"'); !ok {
 				return b, false
 			}
-			// println("\tID", acc.ID)
-		case len(t) > accJoinedLen && string(t[:accJoinedLen]) == accJoinedKey:
-			if acc.Joined, t, ok = parseInt(t[accJoinedLen:]); !ok {
-				return b, false
-			}
-			// println("\tJoined", acc.Joined)
+			// println("\tStatus", string(acc.Status))
 		case len(t) > accPremiumLen && string(t[:accPremiumLen]) == accPremiumKey:
 			if t, ok = parseSymbol(t[accPremiumLen:], '{'); !ok {
 				return b, false
@@ -208,5 +217,6 @@ func (acc *Account) Parse(b []byte) ([]byte, bool) {
 		return b, false
 	}
 	// println("} //Account")
+	//fmt.Println(acc.ID, acc.Birth, acc.Joined)
 	return t, true
 }
