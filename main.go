17f1356007c128dbcd804be0f7ed81967494b904
freepk <freepk@mail.ru>
2019-01-18 01:59:12 +0000

    Filter

--- main.go
+++ main.go
@@ -21,7 +21,50 @@ func AccountsHandler(ctx *fasthttp.RequestCtx, svc *service.AccountsService) {
 	case `GET`:
 		switch string(path) {
 		case `filter/`:
+			errs := 0
+			args := ctx.QueryArgs()
+			args.VisitAll(func(k, v []byte) {
+				switch string(k) {
+				case `limit`:
+				case `query_id`:
+				case `sex_eq`:
+				case `country_eq`:
+				case `country_null`:
+				case `status_eq`:
+				case `status_neq`:
+				case `interests_contains`:
+				case `interests_any`:
+				case `likes_contains`:
+				case `city_eq`:
+				case `city_any`:
+				case `city_null`:
+				case `birth_gt`:
+				case `birth_lt`:
+				case `birth_year`:
+				case `premium_now`:
+				case `premium_null`:
+				case `email_gt`:
+				case `email_lt`:
+				case `email_domain`:
+				case `sname_starts`:
+				case `sname_null`:
+				case `fname_null`:
+				case `fname_any`:
+				case `phone_null`:
+				case `phone_code`:
+				default:
+					errs++
+				}
+			})
+			if errs > 0 {
+				ctx.SetStatusCode(fasthttp.StatusBadRequest)
+				return
+			}
+			ctx.SetStatusCode(fasthttp.StatusOK)
+			return
 			/*
+				//limit
+				//query_id
 				sex_eq
 				country_eq
 				country_null
