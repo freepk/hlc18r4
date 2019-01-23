293afd2af9bda7742c30831acfcff8decb0c1e4f
freepk <freepk@mail.ru>
2019-01-23 23:27:18 +0000

    Inverted Grow

--- inverted/inverted.go
+++ inverted/inverted.go
@@ -1,9 +1,5 @@
 package inverted
 
-import (
-	"fmt"
-)
-
 type Document struct {
 	ID     int
 	Parts  []int
@@ -17,13 +13,14 @@ type Indexer interface {
 
 type Inverted struct {
 	indexer Indexer
+	layout  [][][][]uint32
 }
 
 func NewInverted(indexer Indexer) *Inverted {
 	return &Inverted{indexer: indexer}
 }
 
-func (inv *Inverted) prepare() (layout [][][]uint32) {
+func (inv *Inverted) prepare() (layout [][][]int) {
 	it := inv.indexer
 	it.Reset()
 	for {
@@ -34,15 +31,15 @@ func (inv *Inverted) prepare() (layout [][][]uint32) {
 		for _, part := range doc.Parts {
 			if grow := part + 1 - len(layout); grow > 0 {
 				for i := 0; i < grow; i++ {
-					layout = append(layout, make([][]uint32, len(doc.Tokens)))
+					layout = append(layout, make([][]int, len(doc.Tokens)))
 				}
 			}
-			for index, tokens := range doc.Tokens {
+			for field, tokens := range doc.Tokens {
 				for _, token := range tokens {
-					if grow := token + 1 - len(layout[part][index]); grow > 0 {
-						layout[part][index] = append(layout[part][index], make([]uint32, grow)...)
+					if grow := token + 1 - len(layout[part][field]); grow > 0 {
+						layout[part][field] = append(layout[part][field], make([]int, grow)...)
 					}
-					layout[part][index][token]++
+					layout[part][field][token]++
 				}
 			}
 		}
@@ -51,5 +48,23 @@ func (inv *Inverted) prepare() (layout [][][]uint32) {
 }
 
 func (inv *Inverted) Rebuild() {
-	fmt.Println(inv.prepare())
+	layout := inv.prepare()
+	if grow := len(layout) - len(inv.layout); grow > 0 {
+		inv.layout = append(inv.layout, make([][][][]uint32, grow)...)
+	}
+	for part := range layout {
+		if grow := len(layout[part]) - len(inv.layout[part]); grow > 0 {
+			inv.layout[part] = append(inv.layout[part], make([][][]uint32, grow)...)
+		}
+		for field := range layout[part] {
+			if grow := len(layout[part][field]) - len(inv.layout[part][field]); grow > 0 {
+				inv.layout[part][field] = append(inv.layout[part][field], make([][]uint32, grow)...)
+			}
+			for token := range layout[part][field] {
+				if grow := layout[part][field][token] - len(inv.layout[part][field][token]); grow > 0 {
+					inv.layout[part][field][token] = append(inv.layout[part][field][token], make([]uint32, grow)...)
+				}
+			}
+		}
+	}
 }
