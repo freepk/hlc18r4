33f1935d0ed423c26d690403e283b666d9c7f1da
Pavel Chabinsky <freepk@mail.ru>
2019-01-31 17:00:06 +0300

    City, Sex, Status

---
+++ search/_sex.go
@@ -0,0 +1,34 @@
+package search
+
+import (
+	"github.com/freepk/inverted"
+	"gitlab.com/freepk/hlc18r4/proto"
+	"gitlab.com/freepk/hlc18r4/tokens"
+)
+
+func sexProc(doc *inverted.Document, acc *proto.Account) {
+	switch acc.Sex {
+	case tokens.MaleSex:
+		doc.Parts = append(doc.Parts, tokens.MaleSex)
+	case tokens.FemaleSex:
+		doc.Parts = append(doc.Parts, tokens.FemaleSex)
+	}
+	switch acc.Status {
+	case tokens.SingleStatus:
+		doc.Fields[StatusField] = append(doc.Fields[StatusField], tokens.SingleStatus, tokens.NotInRelStatus, tokens.NotComplStatus)
+	case tokens.InRelStatus:
+		doc.Fields[StatusField] = append(doc.Fields[StatusField], tokens.InRelStatus, tokens.NotSingleStatus, tokens.NotComplStatus)
+	case tokens.ComplStatus:
+		doc.Fields[StatusField] = append(doc.Fields[StatusField], tokens.ComplStatus, tokens.NotSingleStatus, tokens.NotInRelStatus)
+	}
+	if acc.Country > 0 {
+		doc.Fields[CountryField] = append(doc.Fields[CountryField], tokens.NotNull, int(acc.Country))
+	} else {
+		doc.Fields[CountryField] = append(doc.Fields[CountryField], tokens.Null)
+	}
+	if acc.City > 0 {
+		doc.Fields[CityField] = append(doc.Fields[CityField], tokens.NotNull, int(acc.City))
+	} else {
+		doc.Fields[CityField] = append(doc.Fields[CityField], tokens.Null)
+	}
+}
---
+++ search/_status.go
@@ -0,0 +1,34 @@
+package search
+
+import (
+	"github.com/freepk/inverted"
+	"gitlab.com/freepk/hlc18r4/proto"
+	"gitlab.com/freepk/hlc18r4/tokens"
+)
+
+func statusProc(doc *inverted.Document, acc *proto.Account) {
+	switch acc.Status {
+	case tokens.SingleStatus:
+		doc.Parts = append(doc.Parts, tokens.SingleStatus, tokens.NotInRelStatus, tokens.NotComplStatus)
+	case tokens.InRelStatus:
+		doc.Parts = append(doc.Parts, tokens.InRelStatus, tokens.NotSingleStatus, tokens.NotComplStatus)
+	case tokens.ComplStatus:
+		doc.Parts = append(doc.Parts, tokens.ComplStatus, tokens.NotSingleStatus, tokens.NotInRelStatus)
+	}
+	switch acc.Sex {
+	case tokens.MaleSex:
+		doc.Fields[SexField] = append(doc.Fields[SexField], tokens.MaleSex)
+	case tokens.FemaleSex:
+		doc.Fields[SexField] = append(doc.Fields[SexField], tokens.FemaleSex)
+	}
+	if acc.Country > 0 {
+		doc.Fields[CountryField] = append(doc.Fields[CountryField], tokens.NotNull, int(acc.Country))
+	} else {
+		doc.Fields[CountryField] = append(doc.Fields[CountryField], tokens.Null)
+	}
+	if acc.City > 0 {
+		doc.Fields[CityField] = append(doc.Fields[CityField], tokens.NotNull, int(acc.City))
+	} else {
+		doc.Fields[CityField] = append(doc.Fields[CityField], tokens.Null)
+	}
+}
