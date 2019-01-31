950d046d3eba52e85c66a99ea9bf9649053b8f74
Pavel Chabinsky <freepk@mail.ru>
2019-01-31 17:09:51 +0300

    Birth, Joined, Interest

---
+++ search/interest.go
@@ -0,0 +1,40 @@
+package search
+
+import (
+	"github.com/freepk/inverted"
+	"gitlab.com/freepk/hlc18r4/proto"
+	"gitlab.com/freepk/hlc18r4/tokens"
+)
+
+func interestProc(doc *inverted.Document, acc *proto.Account) {
+	for i := range acc.Interests {
+		if acc.Interests[i] == 0 {
+			break
+		}
+		doc.Parts = append(doc.Parts, int(acc.Interests[i]))
+	}
+	switch acc.Sex {
+	case tokens.MaleSex:
+		doc.Fields[SexField] = append(doc.Fields[SexField], tokens.MaleSex)
+	case tokens.FemaleSex:
+		doc.Fields[SexField] = append(doc.Fields[SexField], tokens.FemaleSex)
+	}
+	switch acc.Status {
+	case tokens.SingleStatus:
+		doc.Fields[StatusField] = append(doc.Fields[StatusField], tokens.SingleStatus, tokens.NotInRelStatus, tokens.NotComplStatus)
+	case tokens.InRelStatus:
+		doc.Fields[StatusField] = append(doc.Fields[StatusField], tokens.InRelStatus, tokens.NotSingleStatus, tokens.NotComplStatus)
+	case tokens.ComplStatus:
+		doc.Fields[StatusField] = append(doc.Fields[StatusField], tokens.ComplStatus, tokens.NotSingleStatus, tokens.NotInRelStatus)
+	}
+	if acc.Country > 0 {
+		doc.Fields[CountryField] = append(doc.Fields[CountryField], tokens.NotNull, int(acc.Country))
+	} else {
+		doc.Fields[CountryField] = append(doc.Fields[CountryField], tokens.Null)
+	}
+	if acc.City > 0 {
+		doc.Fields[CityField] = append(doc.Fields[CityField], tokens.NotNull, int(acc.City))
+	} else {
+		doc.Fields[CityField] = append(doc.Fields[CityField], tokens.Null)
+	}
+}
