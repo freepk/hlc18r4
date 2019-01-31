cbbb3a7900e40a379cc50a968107e784997d483f
Pavel Chabinsky <freepk@mail.ru>
2019-01-31 16:47:27 +0300

    Cities

--- search/search.go
+++ search/search.go
@@ -29,24 +29,28 @@ type SearchService struct {
 	rep       *repo.AccountsRepo
 	likes     *LikeIndex
 	countries *inverted.Inverted
-}
-
-func newCountryIndex(rep *repo.AccountsRepo) *inverted.Inverted {
-	proc := NewAccountsProcessor(rep, countryProc, 80, 5)
-	return inverted.NewInverted(proc)
+	cities    *inverted.Inverted
 }
 
 func NewSearchService(rep *repo.AccountsRepo) *SearchService {
 	likes := NewLikeIndex(rep)
-	countries := newCountryIndex(rep)
-	return &SearchService{rep: rep, likes: likes, countries: countries}
+	countries := inverted.NewInverted(NewAccountsProcessor(rep, countryProc, 80, 5))
+	cities := inverted.NewInverted(NewAccountsProcessor(rep, cityProc, 800, 5))
+	return &SearchService{rep: rep, likes: likes, countries: countries, cities: cities}
 }
 
 func (svc *SearchService) Rebuild() {
 	gr := &sync.WaitGroup{}
 	gr.Add(2)
-	go func() { defer gr.Done(); svc.likes.Rebuild() }()
-	go func() { defer gr.Done(); svc.countries.Rebuild() }()
+	go func() {
+		defer gr.Done()
+		svc.likes.Rebuild()
+	}()
+	go func() {
+		defer gr.Done()
+		svc.countries.Rebuild()
+		svc.cities.Rebuild()
+	}()
 	gr.Wait()
 }
 
@@ -54,17 +58,21 @@ func (svc *SearchService) Likes(t int) *LikeIter {
 	return svc.likes.Likes(t)
 }
 
-func (svc *SearchService) Sex(t int) *inverted.TokenIter {
-	return nil
-}
+// Common
 
-func (svc *SearchService) Status(t int) *inverted.TokenIter {
-	return nil
-}
+//func (svc *SearchService) Sex(t int) *inverted.TokenIter {
+//	return nil
+//}
 
-func (svc *SearchService) Country(t int) *inverted.TokenIter {
-	return nil
-}
+//func (svc *SearchService) Status(t int) *inverted.TokenIter {
+//	return nil
+//}
+
+//func (svc *SearchService) Country(t int) *inverted.TokenIter {
+//	return nil
+//}
+
+// Countries
 
 func (svc *SearchService) Countries(t int) *CountryIndex {
 	return &CountryIndex{part: svc.countries.Part(t)}
@@ -83,5 +91,27 @@ func (idx *CountryIndex) Status(t int) *inverted.TokenIter {
 }
 
 func (idx *CountryIndex) Interest(t int) *inverted.TokenIter {
+	return idx.part.Field(InterestField).Token(t).Iter()
+}
+
+// Cities
+
+func (svc *SearchService) Cities(t int) *CityIndex {
+	return &CityIndex{part: svc.cities.Part(t)}
+}
+
+type CityIndex struct {
+	part *inverted.Part
+}
+
+func (idx *CityIndex) Sex(t int) *inverted.TokenIter {
+	return idx.part.Field(SexField).Token(t).Iter()
+}
+
+func (idx *CityIndex) Status(t int) *inverted.TokenIter {
+	return idx.part.Field(StatusField).Token(t).Iter()
+}
+
+func (idx *CityIndex) Interest(t int) *inverted.TokenIter {
 	return idx.part.Field(InterestField).Token(t).Iter()
 }
